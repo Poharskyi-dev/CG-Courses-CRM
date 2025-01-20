775986031ecf81928ab93252b888cda112e39187
Andrii P <andriip@MAC-1F9716.local>
2025-01-20 15:06:43 +0100

    first commit

---
+++ src/test/java/com/opencourse/cgcoursescrm/CgCoursesCrmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.opencourse.cgcoursescrm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CgCoursesCrmApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
