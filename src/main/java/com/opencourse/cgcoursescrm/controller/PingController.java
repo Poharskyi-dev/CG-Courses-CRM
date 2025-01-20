b402383bf18b95e8f6b484813e3f9c59fe6faf17
Andrii P <andriip@MAC-1F9716.local>
2025-01-20 15:42:43 +0100

    POST, GET, DTOs FOR USER + API

---
+++ src/main/java/com/opencourse/cgcoursescrm/controller/PingController.java
@@ -0,0 +1,12 @@
+package com.opencourse.cgcoursescrm.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class PingController {
+    @GetMapping("test-link/ping")
+    public String ping() {
+        return "pong";
+    }
+}
