f97c1895c46be13a021854f188d276e3b329499d
Mykola Digtiar <mykola.digtiar@gmail.com>
2025-03-10 15:39:57 +0100

    Clean-up security config

--- src/main/java/com/opencourse/cgcoursescrm/domain/service/UserServiceImpl.java
+++ src/main/java/com/opencourse/cgcoursescrm/domain/service/UserServiceImpl.java
@@ -15,18 +15,18 @@ import java.util.UUID;
 public class UserServiceImpl implements UserService {
 
     private final UserRepository userRepository;
-    private final PasswordEncoder passwordEncoder;
+    // private final PasswordEncoder passwordEncoder;
 
     @Autowired
-    public UserServiceImpl(UserRepository userRepository, PasswordEncoder passwordEncoder) {
+    public UserServiceImpl(UserRepository userRepository/*, PasswordEncoder passwordEncoder*/) {
         this.userRepository = userRepository;
-        this.passwordEncoder = passwordEncoder;
+        // this.passwordEncoder = passwordEncoder;
     }
 
     @Override
     public User createUser(User user) {
-        String encodedPassword = passwordEncoder.encode(user.getPassword());
-        user.setPassword(encodedPassword);
+//        String encodedPassword = passwordEncoder.encode(user.getPassword());
+//        user.setPassword(encodedPassword);
 
         return userRepository.save(user);
     }
